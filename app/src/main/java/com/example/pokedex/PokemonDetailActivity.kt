f17d03a0b881d071ba550392161343a68e92d1f9
Rin-t <rin.t.4921@gmail.com>
2023-01-02 01:09:43 +0900

    詳細画面から戻る処理を追加

--- app/src/main/java/com/example/pokedex/PokemonDetailActivity.kt
+++ app/src/main/java/com/example/pokedex/PokemonDetailActivity.kt
@@ -2,10 +2,18 @@ package com.example.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PokemonDetailActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokemon_detail)
+
+        val backButton: Button = findViewById(R.id.backButton)
+
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
