b89e88ae199d89e2efc32de6611c896cd330d240
Rin-t <rin.t.4921@gmail.com>
2023-01-02 18:52:46 +0900

    API通信の処理を追加

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.pokedex
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.URL
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,9 +25,52 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecyclerView()
+        setupItemViewClickListener()
+
+        val url = "https://pokeapi.co/api/v2/pokemon/1/"
+        pokemonTask(url)
+    }
+
+    private fun pokemonTask(url: String) {
+        lifecycleScope.launch {
+            val result = pokemonBackGroundTask(url)
+            pokemonJsonTask(result)
+        }
+    }
+
+    private suspend fun pokemonBackGroundTask(url: String): String {
+
+        val response = withContext(Dispatchers.IO) {
+            var httpResult = ""
+            try {
+                val urlObject = URL(url)
+                val reader = BufferedReader(InputStreamReader(urlObject.openStream()))
+                httpResult = reader.readText()
+            } catch (error: IOException) {
+                error.printStackTrace()
+            } catch (error: JSONException) {
+                error.printStackTrace()
+            }
+            return@withContext httpResult
+        }
+        return response
+    }
+
+    private fun pokemonJsonTask(result: String) {
+        val jsonObject = JSONObject(result)
+        val name = jsonObject.getString("name")
+        println(name)
+    }
+
+
+    private fun setupRecyclerView() {
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = GridLayoutManager(this, 2, RecyclerView.VERTICAL, false)
+    }
+
+    private fun setupItemViewClickListener() {
         adapter.setItemViewClickListener(
             object : RecyclerAdapter.OnItemClickListener {
                 override fun onItemClick(pokemon: String) {
